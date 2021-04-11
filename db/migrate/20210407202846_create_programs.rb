4e461b21fdf9a1bb08358b3dc73f26631ea6036f
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-11 13:04:19 -0400

    modify create_programs migration

--- db/migrate/20210407202846_create_programs.rb
+++ db/migrate/20210407202846_create_programs.rb
@@ -1,9 +1,15 @@
 class CreatePrograms < ActiveRecord::Migration[6.1]
   def change
     create_table :programs do |t|
-      t.string :name
+      t.string :program_type
+      t.string :site_name
       t.integer :borough_id
-
+      t.string :agency
+      t.integer :contact_number
+      t.string :age_group
+      t.text :address
+      t.string :capacity
+      
       t.timestamps
     end
   end
