6ef5960ce8dc8ec145808a78cc8a95536e2c5d98
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-11 12:55:46 -0400

    modify create_students migration

--- db/migrate/20210407202309_create_students.rb
+++ db/migrate/20210407202309_create_students.rb
@@ -4,10 +4,19 @@ class CreateStudents < ActiveRecord::Migration[6.1]
       t.string :first_name
       t.string :last_name
       t.integer :date_of_birth
+      t.string :gender
       t.string :username
       t.string :password_digest
-      t.string :school
+      t.string :school_name
       t.integer :grade
+      t.string :address_field_one 
+      t.string :address_field_two
+      t.string :city 
+      t.integer :zip_code 
+      t.string :country
+      t.integer :phone_number
+      t.string :email_address 
+
 
       t.timestamps
     end
