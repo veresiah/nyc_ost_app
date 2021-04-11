aac14dcd6fbdc07061728c0a84f6e66bc5266c76
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-11 15:16:20 -0400

    stub out variables for index, new, create resumes

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,11 +1,19 @@
 class ResumesController < ApplicationController
   def index
+    @resumes = Resume.all
   end
 
   def new
+    @resume = Resume.new
   end
 
   def create
+    @resume = Resume.new(resume_params)
+    if @resume.save   
+      redirect_to resumes_path, notice: "Successfully uploaded."   
+   else   
+      render "new"   
+   end   
   end
 
   def destroy
