35dfa5ba0a56a27799a1f9eceb9c04fae5f317a4
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-12 10:03:54 -0400

    add secure password and DOB validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     enum role: [:parent, :student :provider]
+    has_secure_password 
+    validates :date_of_birth, presence: true, if: lambda {self.role.to_s == 'student'}
 end
