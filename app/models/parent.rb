748f81d4264a6f1a7a71a2fa31357b95b0ec67eb
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-16 10:20:29 -0400

    order model files

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,12 +1,13 @@
 class Parent < ApplicationRecord
     has_secure_password
+
+    has_many :applications 
+    has_many :students, through: :applications
+    has_many :programs, through: :applications 
+
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :username, presence: true, uniqueness: true 
     validates :password, presence: true
     validates :email_address, presence: true, uniqueness: true
-
-    has_many :applications 
-    has_many :students, through: :applications
-    has_many :programs, through: :applications 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,14 +1,15 @@
 class Student < ApplicationRecord
     has_secure_password
+    
+    has_many :applications
+    has_many :parents, through: :applications
+    has_many :programs, through: :applications 
+
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :date_of_birth, presence: true
     validates :username, presence: true, uniqueness: true 
     validates :password, presence: true
     validates :email_address, presence: true, uniqueness: true
-    
-    has_many :applications
-    has_many :parents, through: :applications
-    has_many :programs, through: :applications 
 end
 
