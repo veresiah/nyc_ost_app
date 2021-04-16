b239c2b010504ceda7c908949f19b703f7854fc6
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-16 07:10:54 -0400

    remove resumes, students, parents

--- db/migrate/20210411180611_drop_parents_table.rb
+++
@@ -1,5 +0,0 @@
-class DropParentsTable < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :parents
-  end
-end
--- db/migrate/20210411180725_drop_students_table.rb
+++
@@ -1,5 +0,0 @@
-class DropStudentsTable < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :students
-  end
-end
