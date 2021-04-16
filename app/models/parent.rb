a98c273360ad3ac288107015158cadb81fef48cf
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-16 07:38:09 -0400

    add validattions to parent model

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,8 @@
 class Parent < ApplicationRecord
     has_secure_password
+    validates :username, presence: true, uniqueness: true 
+    validates :password, presence: true
+    
     has_many :applications 
     has_many :students, through: :applications
     has_many :programs, through: :applications 
