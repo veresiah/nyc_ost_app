be8e7f02451a72e53cd2c3a80a5f08a48b28387e
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-04-16 09:47:50 -0400

    add validations to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,11 @@
 class Student < ApplicationRecord
     has_secure_password
+    validates :first_name, presence: true
+    validates :last_name, presence: true
     validates :date_of_birth, presence: true
     validates :username, presence: true, uniqueness: true 
     validates :password, presence: true
+    validates :email_address, presence: true, uniqueness: true
     
     has_many :applications
     has_many :parents, through: :applications
